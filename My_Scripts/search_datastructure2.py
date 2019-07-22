aec100bb877bf00dadf08a3a379949ad4539179c
vinaykumars110 <vinaykumars110@gmail.com>
2019-07-22 13:04:18 +0530

    added new verison of search_python script
    added new verison of search_python script

--- My_Scripts/search_datastructure2.py
+++ My_Scripts/search_datastructure2.py
@@ -1,28 +1,31 @@
 import pprint
 
-def search(d, search_pattern, prev_datapoint_path=[]):
+def search(d, search_pattern, prev_datapoint_path=''):
     output = []
     current_datapoint = d
-    current_datapoint_path = prev_datapoint_path.copy()
+    current_datapoint_path = prev_datapoint_path
     if type(current_datapoint) is dict:
         for dkey in current_datapoint:
             if search_pattern in str(dkey):
-                c = current_datapoint_path.copy()
-                c.append(dkey)
+                c = current_datapoint_path
+                c+="['"+dkey+"']"
                 output.append(c)
-            c = current_datapoint_path.copy()
-            c.append(dkey)
-            output.append(search(current_datapoint[dkey], search_pattern, c))
+            c = current_datapoint_path
+            c+="['"+dkey+"']"
+            for i in search(current_datapoint[dkey], search_pattern, c):
+                output.append(i)
     elif type(current_datapoint) is list:
         for i in range(0, len(current_datapoint)):
             if search_pattern in str(i):
-                output.append(current_datapoint_path.append(i))
-            c = current_datapoint_path.copy()
-            c.append(i)
-            output.append(search(current_datapoint[i], search_pattern, c))
+                c = current_datapoint_path
+                c += "[" + str(i) + "]"
+                output.append(i)
+            c = current_datapoint_path
+            c+="["+ str(i) +"]"
+            for i in search(current_datapoint[i], search_pattern, c):
+                output.append(i)
     elif search_pattern in str(current_datapoint):
-        c = current_datapoint_path.copy()
-        c.append(current_datapoint)
+        c = current_datapoint_path
         output.append(c)
     output = filter(None, output)
     return list(output)
@@ -82,6 +85,46 @@ if __name__ == "__main__":
             }
     }
 
-    d3 = {"status":"success","data":{"equity":{"enabled":"true","net":11874.79,"available":{"adhoc_margin":0,"cash":11874.79,"collateral":0,"intraday_payin":0},"utilised":{"debits":0,"exposure":0,"m2m_realised":0,"m2m_unrealised":0,"option_premium":0,"payout":0,"span":0,"holding_sales":0,"turnover":0}},"commodity":{"enabled":"true","net":0,"available":{"adhoc_margin":0,"cash":0,"collateral":0,"intraday_payin":0},"utilised":{"debits":0,"exposure":0,"m2m_realised":0,"m2m_unrealised":0,"option_premium":0,"payout":0,"span":0,"holding_sales":0,"turnover":0}}}}
-    pprint.pprint(search(d3, 'true', ['d3']))
-    print(d3['data']['equity']['net'])
+    pprint.pprint(search(d,'svg+xml','d'))
+    pprint.pprint(search(d2,'500','d2'))
+    d3 = {"status":"success",
+          "data":{
+              "equity":{
+                  "enabled":"true",
+                  "net":11874.79,
+                  "available":{"adhoc_margin":0,
+                                 "cash":11874.79,
+                                 "collateral":0,
+                                 "intraday_payin":0},
+                  "utilised":{"debits":0,
+                                "exposure":0,
+                                "m2m_realised":0,
+                                "m2m_unrealised":0,
+                                "option_premium":0,
+                                "payout":0,
+                                "span":0,
+                                "holding_sales":0,
+                                "turnover":0}},
+              "commodity":{
+                  "enabled":"true",
+                  "net":0,
+                  "available":{"adhoc_margin":0,
+                                 "cash":0,
+                                 "collateral":0,
+                                 "intraday_payin":0},
+                  "utilised":{"debits":0,
+                               "exposure":0,
+                               "m2m_realised":0,
+                               "m2m_unrealised":0,
+                               "option_premium":0,
+                               "payout":0,
+                               "span":0,
+                               "holding_sales":0,
+                               "turnover":0}}
+          }
+          }
+    #d3 = #{"status": "success",
+
+    d3 = {'a':'ab','c':[{'b':'ab'}]}
+    #pprint.pprint(search(d2, 'id', 'd2'))
+    #print(d3['data']['equity']['net'])
